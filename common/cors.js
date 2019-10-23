dd2f4fdc40c654b92baf0e9098f2ccf705c6840d
Pham Tien Hung <phhung93@gmail.com>
2019-10-23 10:04:05 +0700

    restart heroku

--- common/cors.js
+++ common/cors.js
@@ -1,5 +1,5 @@
 const cors = require('cors');
-const allowedOrigins = ['http://localhost:8080', 'https://sheltered-badlands-02083.herokuapp.com'];
+const allowedOrigins = ['localhost:8080', 'sheltered-badlands-02083.herokuapp.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
